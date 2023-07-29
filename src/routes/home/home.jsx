1c58d6308b693851f1af58e6000a37e262352791
OngkyRinaldo <edoorinaldoo@gmail.com>
2023-07-29 15:40:41 +0700

    update navbar

--- src/routes/home/home.jsx
+++ src/routes/home/home.jsx
@@ -1,7 +1,74 @@
 import React from 'react';
 
 const Home = () => {
-    return <div> Home</div>;
+    return (
+        <div>
+            {' '}
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
+                est quasi cumque totam eveniet iste asperiores et fuga illum!
+                Modi ipsam eligendi eos, nisi laborum veniam reprehenderit
+                dignissimos fugiat illo perferendis minima, tempora sint tempore
+                aut ratione enim est beatae ipsum numquam quia quam? Nostrum
+                alias iure officiis dolore ex! Possimus illo, voluptatum, omnis
+                accusantium eaque perferendis alias maiores saepe optio ipsa
+                laborum dolore soluta ad perspiciatis inventore obcaecati ab
+                reprehenderit modi veniam! Officiis, accusamus incidunt autem
+                sapiente deserunt quia cumque sequi, et error, eveniet
+                laudantium magnam tempora praesentium fuga. Numquam tempore
+                vitae ratione sunt? Quasi mollitia architecto, ab iusto corrupti
+                quibusdam. Beatae numquam quisquam ut quam labore. Incidunt
+                autem impedit tenetur, quisquam aliquid necessitatibus delectus
+                illo, dolor neque eum quis. Ducimus et impedit magni reiciendis
+                voluptates praesentium, vero pariatur minus delectus recusandae,
+                adipisci accusamus debitis molestias perspiciatis ratione sint
+                dolor expedita molestiae quaerat eligendi voluptatibus! Quos
+                iure corrupti reiciendis eius doloribus repudiandae, modi a?
+                Tenetur omnis porro qui velit quam cum similique iusto quis
+                natus earum, expedita blanditiis! Ut ipsam porro, sit, eaque
+                quas velit debitis laboriosam nihil ullam beatae quibusdam! Vero
+                ab sapiente velit ducimus ad sed blanditiis nemo doloribus animi
+                earum obcaecati asperiores fugit ea aut quos iste magni, hic
+                tempore odio nostrum non nobis. Tempora officia, corporis
+                reiciendis ullam, porro molestias illo aspernatur ipsa voluptas
+                cum vel corrupti consectetur perferendis cumque esse? Nam
+                corporis eaque expedita ipsa sapiente. Soluta est placeat illo
+                eveniet ratione fugit eum pariatur nostrum facere, eos, sit,
+                distinctio necessitatibus iure? Culpa veritatis, magnam incidunt
+                et minima in iste? Deserunt quis culpa inventore quod.
+                Distinctio, quaerat voluptatem! Iure suscipit illum quisquam
+                perferendis delectus porro vel at rerum et voluptate nesciunt
+                eveniet impedit excepturi consectetur iste architecto deleniti
+                officia, sequi quae culpa animi accusantium. Illum, temporibus
+                incidunt asperiores, iusto dicta expedita obcaecati architecto
+                inventore autem delectus nulla? Tempora tempore fuga mollitia
+                ducimus, eius enim saepe earum expedita consequuntur, facere
+                illum explicabo officiis! Recusandae architecto nihil nisi
+                molestiae perferendis sequi praesentium voluptas voluptatum id?
+                Architecto aut doloribus reiciendis temporibus nostrum nam
+                magnam odio ad rerum laboriosam atque, nesciunt tempora ullam
+                dolor. Id odit cupiditate saepe amet molestiae deserunt rem,
+                iusto quos fuga illum debitis eos impedit. Exercitationem
+                pariatur animi eligendi eum perferendis vel recusandae nemo,
+                suscipit quae placeat mollitia alias at excepturi odio et sunt
+                sit! Nihil neque vero ipsum aspernatur ex dolorem maiores atque
+                asperiores odit nesciunt eveniet quidem praesentium laudantium
+                hic, iste expedita sed provident delectus, dignissimos eligendi
+                error. Sunt praesentium necessitatibus dolor quia fuga laborum
+                tempora vel asperiores facere eum modi, quas impedit doloribus
+                eaque? Id maiores aut nulla odit unde quod doloribus, veniam
+                nobis molestiae pariatur saepe fugit reprehenderit dicta, amet
+                enim eligendi voluptate dolore itaque consequuntur ipsum
+                blanditiis facilis. Vitae facere placeat saepe porro ex unde
+                dolorem quasi doloremque est quis distinctio, quam enim impedit
+                pariatur veritatis fuga harum voluptates! Nemo accusamus
+                exercitationem dolor mollitia suscipit doloremque provident
+                commodi nulla a quidem qui eum, iusto expedita porro saepe non
+                neque, at totam corrupti veniam. Illum magni rem omnis iure
+                quibusdam?
+            </p>
+        </div>
+    );
 };
 
 export default Home;
--- src/routes/navigation/navigation.jsx
+++ src/routes/navigation/navigation.jsx
@@ -1,7 +1,7 @@
-import { Link } from 'react-router-dom';
-import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
+import { Link, NavLink } from 'react-router-dom';
+import Logo from '../../assets/logo.png';
 import './navigation.styles.scss';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { UserContext } from '../../context/user.context';
 import { signOutUser } from '../../utils/firebase/firebase.utils';
 import CartIcon from '../../component/cart-icon/cart-icon.component';
@@ -11,32 +11,75 @@ import CartDropdown from '../../component/cart-dropdown/cart-dropdown.component'
 const Navigation = () => {
     const { currentUser } = useContext(UserContext);
     const { isCartOpen } = useContext(CartContext);
+
+    const [clicked, setClicked] = useState(false);
+
+    const responsiveHandler = () => setClicked(!clicked);
     return (
         <header>
             <nav>
                 <Link className='logo-container' to='/'>
-                    <CrwnLogo />
+                    <img src={Logo} alt='logo' />
                 </Link>
 
                 <div className='nav-links-container'>
-                    <Link className='nav-link' to='/shop'>
-                        shop
-                    </Link>
-
+                    <ul id='navbar2' className={clicked ? 'activeNav' : ''}>
+                        <li>
+                            <NavLink className='nav-link' to='/'>
+                                Home
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink className='nav-link' to='/shop'>
+                                Categories
+                            </NavLink>
+                        </li>
+                        <li>
+                            {currentUser ? (
+                                <span
+                                    className='nav-link'
+                                    onClick={signOutUser}
+                                >
+                                    SIGN OUT
+                                </span>
+                            ) : (
+                                <NavLink className='nav-link' to='/auth'>
+                                    SIGN IN
+                                </NavLink>
+                            )}
+                        </li>
+                        <li>
+                            <CartIcon />
+                        </li>
+                    </ul>
+                    {/* <NavLink className='nav-link' to='/'>
+                        Home
+                    </NavLink>
+                    <NavLink className='nav-link' to='/shop'>
+                        Categories
+                    </NavLink>
                     {currentUser ? (
                         <span className='nav-link' onClick={signOutUser}>
                             SIGN OUT
                         </span>
                     ) : (
-                        <Link className='nav-link' to='/auth'>
+                        <NavLink className='nav-link' to='/auth'>
                             SIGN IN
-                        </Link>
+                        </NavLink>
                     )}
 
-                    <CartIcon />
+                    <CartIcon /> */}
                 </div>
 
                 {isCartOpen && <CartDropdown />}
+
+                <div className='mobile'>
+                    <i
+                        id='bar'
+                        className={clicked ? 'fas fa-times' : 'fas fa-bars'}
+                        onClick={responsiveHandler}
+                    ></i>
+                </div>
             </nav>
         </header>
     );
