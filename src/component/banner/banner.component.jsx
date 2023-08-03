c3510255c7bfd0a8627d728b83c60ac1d8e03b75
OngkyRinaldo <edoorinaldoo@gmail.com>
2023-08-03 12:27:38 +0700

    added component and styles banner for home pages

--- src/component/banner/banner.component.jsx
+++ src/component/banner/banner.component.jsx
@@ -1,7 +1,31 @@
 import './banner.styles.scss';
 
+import Banner1 from '../../assets/img/banner/banner1.jpg';
+import { Link } from 'react-router-dom';
+
 const Banner = () => {
-    return <div>Banner</div>;
+    return (
+        <div className='banner'>
+            <div className='banner-container'>
+                <div className='text-side'>
+                    <div className='text'>
+                        <h2>Creative harmonious living</h2>
+                        <p>
+                            All Products are all made to standard sizes so that
+                            you can mix and match them freely
+                        </p>
+
+                        <Link>
+                            <button>Shop Now</button>
+                        </Link>
+                    </div>
+                </div>
+                <div className='img-side'>
+                    <img src={Banner1} alt='banner' />
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Banner;
--- src/component/homeProducts/homeProducts.component.jsx
+++ src/component/homeProducts/homeProducts.component.jsx
@@ -5,7 +5,7 @@ import './homeProducts.styles.scss';
 const HomeProducts = () => {
     return (
         <div className='popular-container'>
-            <h2 className='popular-h2'>products we are proud of</h2>
+            <h2 className='popular-h2'>Products we are proud of</h2>
             <div className='container'>
                 <div className='products-grid'>
                     <HomeItem />
