9d21c77064cd7c98865c7d09edcd53b16f633239
OngkyRinaldo <edoorinaldoo@gmail.com>
2023-08-05 13:41:24 +0700

    added not found pages

--- src/component/notfound/notfound.component.jsx
+++ src/component/notfound/notfound.component.jsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import './notfound.styles.scss';
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
-    return <div> 404 NotFound</div>;
+    return (
+        <div className='container-not-found'>
+            <div className='found-text'>
+                <h2>Page Not Found</h2>
+                <Link to='/'> Back To Home Page</Link>
+            </div>
+        </div>
+    );
 };
 
 export default NotFound;
