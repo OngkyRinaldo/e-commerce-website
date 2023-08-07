37c3f4d1fb0261f5daaf9eb44a0fa51c97979a5f
OngkyRinaldo <edoorinaldoo@gmail.com>
2023-08-07 15:46:42 +0700

    update

---
+++ src/routes/cart/cart.component.jsx
@@ -0,0 +1,35 @@
+import { useContext } from 'react';
+import './checkout.styles.scss';
+import { CartContext } from '../../context/cartContext';
+import CartItem from '../../component/cart-item/cart-item.component';
+
+const Cart = () => {
+    const { cartItems, cartTotal } = useContext(CartContext);
+    return (
+        <div className='checkout-container'>
+            <table>
+                <thead>
+                    <tr>
+                        <th>No.</th>
+                        <th>Product</th>
+                        <th>Title</th>
+                        <th>Quantity</th>
+                        <th>Price</th>
+                        <th>Remove</th>
+                    </tr>
+                </thead>
+                {cartItems.map((cartItem, index) => (
+                    <CartItem
+                        key={cartItem.id}
+                        cartItem={cartItem}
+                        index={index}
+                    />
+                ))}
+            </table>
+
+            <div className='total'>TOTAL : ${cartTotal} </div>
+        </div>
+    );
+};
+
+export default Cart;
